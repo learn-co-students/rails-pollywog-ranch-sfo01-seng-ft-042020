59641714014e3a145d15975aa5fce0f6225335b4
aa1b02ab7492d696da6e8ac540ffa187ba623c88e15238d8d026f79bc5d39b3c <darrickpang@DESKTOP-02GI1R6.localdomain>
2020-06-03 11:06:43 -0700

    Done.

--- db/migrate/20140922151423_create_frogs.rb
+++ db/migrate/20140922151423_create_frogs.rb
@@ -1,3 +1,6 @@
 class CreateFrogs < ActiveRecord::Migration
   # code goes here
+  t.string :name
+  t.string :color
+  t.integer :pond_id
 end
--- db/migrate/20140922151444_create_tadpoles.rb
+++ db/migrate/20140922151444_create_tadpoles.rb
@@ -1,3 +1,6 @@
 class CreateTadpoles < ActiveRecord::Migration
   # code goes here
+  t.string :name
+  t.string :color
+  t.integer :frog_id
 end
